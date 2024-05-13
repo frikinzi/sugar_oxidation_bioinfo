e42796a8506b6939c1faac32761852c3ae3ab51a
frikinzi <86448602+frikinzi@users.noreply.github.com>
2024-05-13 15:52:57 -0400

    new code

---
+++ taxon_distribution_analyses/fasttree_pipeline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+module load FastTree
+module load clustalw
+
+set -e
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+file=$1
+# Input file containing sequences
+input_file=$file
+
+# Output files
+aligned_file=${file%.*}_aligned.fasta
+trimmed_file=${aligned_file%.*}_aligned_cleaned.fasta
+
+# Perform alignment with MAFFT
+clustalw $input_file -OUTPUT=FASTA -OUTFILE=$aligned_file
+
+# Trim the alignment with Goalign
+goalign clean sites --char=GAP -c 0.97 -i $aligned_file -o $trimmed_file
+
+# Run IQ-TREE on the trimmed alignment
+FastTreeMP "$trimmed_file" > "$trimmed_file".tree
