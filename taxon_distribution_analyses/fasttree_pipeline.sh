ba2122a59cc5ed1a5fc16bf73738d373d3558d19
Angela Jiang <86448602+frikinzi@users.noreply.github.com>
2024-05-21 12:08:42 -0400

    Update fasttree_pipeline.sh

--- taxon_distribution_analyses/fasttree_pipeline.sh
+++ taxon_distribution_analyses/fasttree_pipeline.sh
@@ -6,18 +6,17 @@ set -e
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 file=$1
-# Input file containing sequences
 input_file=$file
 
 # Output files
 aligned_file=${file%.*}_aligned.fasta
 trimmed_file=${aligned_file%.*}_aligned_cleaned.fasta
 
-# Perform alignment with MAFFT
+# Perform alignment with clustalw
 clustalw $input_file -OUTPUT=FASTA -OUTFILE=$aligned_file
 
 # Trim the alignment with Goalign
 goalign clean sites --char=GAP -c 0.97 -i $aligned_file -o $trimmed_file
 
-# Run IQ-TREE on the trimmed alignment
+# Run FastTree on the trimmed alignment
 FastTreeMP "$trimmed_file" > "$trimmed_file".tree
